1551345fe8b2e9c45cfae007ec526aa704fac96e
breadfan69 <breadfan69@example.com>
2026-02-12 19:44:12 -0500

    Update shutdown summary test for mean metrics

--- tests/test_audio_engine_shutdown_summary.py
+++ tests/test_audio_engine_shutdown_summary.py
@@ -19,12 +19,15 @@ class TestAudioEngineShutdownSummary(unittest.TestCase):
         _, kwargs = log_event_mock.call_args
         self.assertEqual(kwargs["raw_rms_min"], "0.100000")
         self.assertEqual(kwargs["raw_rms_max"], "0.400000")
+        self.assertEqual(kwargs["raw_rms_mean"], "0.250000")
         self.assertEqual(kwargs["raw_rms_span"], "0.300000")
         self.assertEqual(kwargs["band_energy_min"], "0.200000")
         self.assertEqual(kwargs["band_energy_max"], "0.700000")
+        self.assertEqual(kwargs["band_energy_mean"], "0.450000")
         self.assertEqual(kwargs["band_energy_span"], "0.500000")
         self.assertEqual(kwargs["flux_min"], "0.3000")
         self.assertEqual(kwargs["flux_max"], "1.1000")
+        self.assertEqual(kwargs["flux_mean"], "0.7000")
         self.assertEqual(kwargs["flux_span"], "0.8000")
 
     def test_shutdown_summary_no_frames_no_log(self):
